f34f5281c16bfca8f056b751dd7f7826fab9bada
LuQuintas11 <luciaquintas14@gmail.com>
2022-10-30 06:37:04 +0000

    Initial commit

---
+++ hospital.py
@@ -0,0 +1,11 @@
+sintomas=[];
+numero=;
+input("Cual es tu nombre:");
+input("Tenes algunos de los siguientes sintomas: 1.Fiebre 2. Vomitos 3. Dificultad Respiratoria")
+
+if sintomas is/and 1 or/and 2 or/and 3:
+    numero="numero prioritario"
+else:
+    numero=" numero no prioritario"
+
+
