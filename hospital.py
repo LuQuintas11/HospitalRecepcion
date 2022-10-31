68d6f97b4b308eea56a956fb9c8272af950135d3
LuQuintas11 <luciaquintas14@gmail.com>
2022-10-31 02:41:30 +0000

    set while true function

--- hospital.py
+++ hospital.py
@@ -2,16 +2,17 @@ import random
 sint=[];
 numero=""
 input("Cual es tu nombre:");
-sint.append(input("Tenes algunos de los siguientes sintomas: 1.Fiebre  2.Vomitos  3.Dificultad Respiratoria 4. No tengo ninguno de estos sintomas"))
-print('Ademas posees alguno otro sintomas')
-sint.append(input("Tenes algunos de los siguientes sintomas: 1.Fiebre  2.Vomitos  3.Dificultad Respiratoria 4. No tengo ninguno de estos sintomas"))
-print('Ademas posees alguno otro sintomas')
-sint.append(input("Tenes algunos de los siguientes sintomas: 1.Fiebre  2.Vomitos  3.Dificultad Respiratoria 4. No tengo ninguno de estos sintomas"))
+
+while True:
+    sint.append(input("Tenes algunos de los siguientes sintomas: 1.Fiebre  2.Vomitos  3.Dificultad Respiratoria 4. No tengo ninguno de estos sintomas"))
+    print('Ademas posees alguno otro sintomas')
+    if '4' in sint:
+        break;
 print(sint)
-if sint == ['1'] and sint == ['2'] and sint == ['3']:
+if len(sint)>=3:
     numero=print(random.randint(0,1000))
     """en google sheet al medico le va a aparecer como paciente super prioritario"""
-elif sint == ['1'] and sint == ['2'] and sint == ['3']:
+elif len(sint)==2:
     numero=print(random.randint(1000,3000))
 else:
     numero=print(random.randint(3000,5000))
